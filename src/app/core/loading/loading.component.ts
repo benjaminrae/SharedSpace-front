0db01064baf4a308247f50b614398c3574a8b371
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-23 11:01:19 +0100

    Update imports in loading component

--- src/app/core/loading/loading.component.ts
+++ src/app/core/loading/loading.component.ts
@@ -1,13 +1,4 @@
 import { Component } from "@angular/core";
-import { type Observable } from "rxjs";
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import { Store } from "@ngrx/store";
-import {
-  selectIsLoading,
-  reducer,
-} from "src/app/store/ui-feature/ui-feature.reducer";
-import { type ApplicationState } from "src/app/store/types";
-import { map } from "rxjs";
 
 @Component({
   selector: "app-loading",
