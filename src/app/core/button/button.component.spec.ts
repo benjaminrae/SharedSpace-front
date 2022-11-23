73c9adf789d5f93f6581c8ecf82346ab1a3750b7
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-23 13:42:25 +0100

    Add test for button onClick

--- src/app/core/button/button.component.spec.ts
+++ src/app/core/button/button.component.spec.ts
@@ -1,12 +1,13 @@
-import { render, screen } from "@testing-library/angular";
+import { EventEmitter } from "@angular/core";
+import { render, screen, fireEvent } from "@testing-library/angular";
 import { ButtonComponent } from "./button.component";
 
 describe("Given a Button component", () => {
+  const buttonText = "Hello";
+  const buttonRole = "button";
+
   describe("When it is rendered with text 'Hello'", () => {
     test("Then it should show a button with text 'Hello' on the screen", async () => {
-      const buttonText = "Hello";
-      const buttonRole = "button";
-
       await render(ButtonComponent, {
         componentProperties: { inverted: false, text: buttonText },
       });
@@ -16,6 +17,28 @@ describe("Given a Button component", () => {
       });
 
       expect(renderedButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered with an onClick action", () => {
+    test("Then the action should be invoked when the user clicks the button", async () => {
+      const onClick = { emit: jest.fn() };
+
+      await render(ButtonComponent, {
+        componentProperties: {
+          inverted: false,
+          text: buttonText,
+          onClick: onClick as unknown as EventEmitter<any>,
+        },
+      });
+
+      const renderedButton = screen.queryByRole(buttonRole, {
+        name: buttonText,
+      });
+
+      fireEvent.click(renderedButton!);
+
+      expect(onClick.emit).toHaveBeenCalled();
     });
   });
 });
