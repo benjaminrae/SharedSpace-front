f2787fcf77279663a3f2c2c289d9148b91b7a018
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-23 17:05:40 +0100

    Add test for login form elements

--- src/app/components/login-form/login-form.component.spec.ts
+++ src/app/components/login-form/login-form.component.spec.ts
@@ -1,23 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { render, screen } from "@testing-library/angular";
+import { FormBuilder } from "@angular/forms";
+import { LoginFormComponent } from "./login-form.component";
+import { ButtonComponent } from "../../core/button/button.component";
 
-import { LoginFormComponent } from './login-form.component';
+describe("Given a LoginFormComponent", () => {
+  const usernameLabel = /username/i;
+  const passwordLabel = /password/i;
+  const loginButtonText = /login/i;
 
-describe('LoginFormComponent', () => {
-  let component: LoginFormComponent;
-  let fixture: ComponentFixture<LoginFormComponent>;
+  describe("When it is rendered", () => {
+    test("Then it should show a heading level 2 with 'Log in to SharedSpace', two inputs 'Username' and 'Password' and a button 'Login'", async () => {
+      const expectedHeading = {
+        level: 2,
+        name: "Log in to SharedSpace",
+      };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
-    })
-    .compileComponents();
+      await render(LoginFormComponent, {
+        providers: [FormBuilder],
+        declarations: [ButtonComponent],
+      });
 
-    fixture = TestBed.createComponent(LoginFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      const usernameInput = screen.queryByRole("textbox", {
+        name: usernameLabel,
+      });
+      const passwordInput = screen.queryByLabelText(passwordLabel);
+      const renderedHeading = screen.queryByRole("heading", expectedHeading);
+      const loginButton = screen.queryByRole("button", {
+        name: loginButtonText,
+      });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+      screen.debug();
+
+      expect(usernameInput).toBeInTheDocument();
+      expect(passwordInput).toBeInTheDocument();
+      expect(renderedHeading).toBeInTheDocument();
+      expect(loginButton).toBeInTheDocument();
+    });
   });
 });
