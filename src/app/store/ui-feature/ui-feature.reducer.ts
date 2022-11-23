90e4b6b8e5c445dea85226d7414861af6490f1c4
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-23 17:57:35 +0100

    Add reducer case for showModal action

--- src/app/store/ui-feature/ui-feature.reducer.ts
+++ src/app/store/ui-feature/ui-feature.reducer.ts
@@ -1,9 +1,13 @@
 import { createFeature, createReducer, on } from "@ngrx/store";
 import mockInitialUiState from "../../mocks/states/mockInitialUiState";
 import { type UiState } from "./types";
-import { hideLoading, showLoading } from "./ui-feature.actions";
+import { hideLoading, showLoading, showModal } from "./ui-feature.actions";
 
-const intiialUiState: UiState = mockInitialUiState;
+const intiialUiState: UiState = {
+  isLoading: false,
+  showModal: false,
+  modalInformation: { isError: false, modalText: "" },
+};
 
 export const uiFeature = createFeature({
   name: "ui",
@@ -23,6 +27,15 @@ export const uiFeature = createFeature({
       (currentState): UiState => ({
         ...currentState,
         isLoading: false,
+      })
+    ),
+
+    on(
+      showModal,
+      (currentState, { payload }): UiState => ({
+        ...currentState,
+        showModal: true,
+        modalInformation: payload,
       })
     )
   ),
