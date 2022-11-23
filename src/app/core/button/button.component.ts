0a2b811384895aec23c3880acab3d90e5aa6ab9f
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-23 16:28:13 +0100

    Update button to include big class and disabled

--- src/app/core/button/button.component.ts
+++ src/app/core/button/button.component.ts
@@ -1,15 +1,30 @@
-import { EventEmitter, Output, Component, Input } from "@angular/core";
+import { EventEmitter, Output, Component, Input, OnInit } from "@angular/core";
 @Component({
   selector: "app-button",
   templateUrl: "./button.component.html",
   styleUrls: ["./button.component.scss"],
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Input() inverted!: boolean;
+  @Input() big!: boolean;
   @Input() text = "Click here";
+  @Input() type = "submit";
+  @Input() disabled!: boolean;
   @Output() onClick = new EventEmitter<any>();
+
+  className!: string;
 
   onClickButton(event: Event) {
     this.onClick.emit(event);
+  }
+
+  ngOnInit(): void {
+    this.className = this.getClassName();
+  }
+
+  private getClassName() {
+    return `button${this.inverted ? " button--inverted" : ""}${
+      this.big ? " button--big" : ""
+    }`;
   }
 }
