5f688bdf361c922b5d613350078375d656cd469a
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-23 11:29:56 +0100

    Remove unused reducers import in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from "./app.component";
 import { StoreModule } from "@ngrx/store";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { environment } from "../environments/environment";
-import { reducers, metaReducers } from "./store";
+import { metaReducers } from "./store";
 import { EffectsModule } from "@ngrx/effects";
 import { CoreModule } from "./core/core.module";
 import { uiFeature } from "./store/ui-feature/ui-feature.reducer";
