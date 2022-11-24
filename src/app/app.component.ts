7b850a4365ea6451b3057dbed588930f4f68ef22
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-24 12:53:11 +0100

    Add token check on page load in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { type Observable } from "rxjs";
+import { TokenService } from "./services/token/token.service";
 import { UiService } from "./services/ui/ui.service";
+import { UserService } from "./services/user/user.service";
 
 @Component({
   selector: "app-root",
@@ -8,12 +10,30 @@ import { UiService } from "./services/ui/ui.service";
   styleUrls: ["./app.component.scss"],
   providers: [UiService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isLoading$: Observable<boolean>;
   showModal$: Observable<boolean>;
 
-  constructor(private readonly uiService: UiService) {
+  constructor(
+    private readonly uiService: UiService,
+    private readonly userService: UserService,
+    private readonly tokenService: TokenService
+  ) {
     this.isLoading$ = this.uiService.getLoadingValue();
     this.showModal$ = this.uiService.getShowModalValue();
+  }
+
+  ngOnInit(): void {
+    this.checkLoggedUser();
+  }
+
+  private checkLoggedUser() {
+    const token = this.tokenService.retrieveToken();
+
+    if (token) {
+      const user = this.tokenService.decodeToken(token);
+
+      this.userService.loginUser({ ...user, token });
+    }
   }
 }
