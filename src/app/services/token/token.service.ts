c4361bb735d3202fafdb4762a703ecb0717d668a
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-24 12:52:21 +0100

    Add store token and retrieve token methods to token service

--- src/app/services/token/token.service.ts
+++ src/app/services/token/token.service.ts
@@ -6,7 +6,19 @@ import { CustomTokenPayload } from "./types";
   providedIn: "root",
 })
 export class TokenService {
+  private get tokenProperty() {
+    return "token";
+  }
+
   decodeToken(token: string): CustomTokenPayload {
     return jwtDecode<CustomTokenPayload>(token);
+  }
+
+  storeToken(token: string) {
+    localStorage.setItem(this.tokenProperty, token);
+  }
+
+  retrieveToken() {
+    return localStorage.getItem(this.tokenProperty);
   }
 }
