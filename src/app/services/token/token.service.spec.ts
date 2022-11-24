9b5808cafc5837b97e4af38770d89366a954c4a8
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-24 22:59:54 +0100

    Add test for decodeToken method in token service

--- src/app/services/token/token.service.spec.ts
+++ src/app/services/token/token.service.spec.ts
@@ -13,7 +13,7 @@ describe("Given the service TokenService", () => {
   const tokenService = new TokenService();
   const tokenProperty = "token";
 
-  describe("When it's method store token is invoked with '1234'", () => {
+  describe("When its method store token is invoked with '1234'", () => {
     test("Then '1234' should be stored in local storage", () => {
       const token = "1234";
 
@@ -23,7 +23,7 @@ describe("Given the service TokenService", () => {
     });
   });
 
-  describe("When it's method retrieve token is invoked and the token 'testtoken' is in localStorage", () => {
+  describe("When its method retrieve token is invoked and the token 'testtoken' is in localStorage", () => {
     const token = "testtoken";
 
     beforeEach(() => {
@@ -34,6 +34,23 @@ describe("Given the service TokenService", () => {
       const retrievedToken = tokenService.retrieveToken();
 
       expect(retrievedToken).toBe(token);
+    });
+  });
+
+  describe("When its method decodeToken is invoked with a token that has username 'admin' and id 'testid' in the payload", () => {
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOiJ0ZXN0aWQifQ.O2baxQ7ITyRYos-PfeswyIx4tyA3_OviE1nR5Ytgoxo";
+
+    test("Then it should return those properties", () => {
+      const idProperty = "id";
+      const usernameProperty = "username";
+      const username = "admin";
+      const id = "testid";
+
+      const decodedToken = tokenService.decodeToken(token);
+
+      expect(decodedToken).toHaveProperty(usernameProperty, username);
+      expect(decodedToken).toHaveProperty(idProperty, id);
     });
   });
 });
