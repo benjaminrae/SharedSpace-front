e3b38156c8d4a9d87e67d3dcd3e5fefd5cddc80b
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-24 12:37:58 +0100

    Create user service with getToken post request

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-
+import { environment } from "src/environments/environment";
+import { User, UserCredentials } from "../../store/user-feature/types";
+import { TokenResponse } from "./types";
+import { Store } from "@ngrx/store";
+import { loginUser } from "src/app/store/user-feature/user-feature.actions";
 @Injectable({
   providedIn: "root",
 })
 export class UserService {
-  constructor(private readonly http: HttpClient) {}
+  private get userLoginPath() {
+    return "/users/login";
+  }
 
-  getToken() {
-    return this.http.post();
+  constructor(
+    private readonly http: HttpClient,
+    private readonly store: Store
+  ) {}
+
+  getToken(loginFormData: UserCredentials) {
+    return this.http.post<TokenResponse>(
+      `${environment.apiUrl}${this.userLoginPath}`,
+      loginFormData
+    );
+  }
+
+  loginUser(userData: User) {
+    this.store.dispatch(loginUser({ payload: userData }));
   }
 }
