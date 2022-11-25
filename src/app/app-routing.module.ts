ad298286ddd010d363cf53af658aa72b474e6364
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-25 16:29:23 +0100

    Update link in login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, type Routes } from "@angular/router";
+import { RegisterFormComponent } from "./components/register-form/register-form.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" },
   { path: "login", component: LoginPageComponent },
+  { path: "register", component: RegisterFormComponent },
 ];
 
 @NgModule({
