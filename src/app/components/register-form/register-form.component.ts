692abc69b8ac00b232a132810c7b832bf8a79642
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-25 17:30:27 +0100

    Create register form with form builder and connect to html

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,7 +1,21 @@
 import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-register-form",
   templateUrl: "./register-form.component.html",
 })
-export class RegisterFormComponent {}
+export class RegisterFormComponent {
+  registerForm = this.fb.group({
+    username: ["", [Validators.required, Validators.minLength(5)]],
+    password: ["", [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ["", [Validators.required, Validators.minLength(8)]],
+    owner: [false],
+  });
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  onSubmit() {
+    return this.registerForm.value;
+  }
+}
