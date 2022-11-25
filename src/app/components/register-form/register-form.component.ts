8d654f80c76e6e0d272dd892ceabaf03abf38ebe
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-25 16:58:52 +0100

    Remove style file and declaration from register from

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,10 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-register-form',
-  templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  selector: "app-register-form",
+  templateUrl: "./register-form.component.html",
 })
-export class RegisterFormComponent {
-
-}
+export class RegisterFormComponent {}
