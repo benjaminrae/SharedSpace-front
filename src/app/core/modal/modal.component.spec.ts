915b37e72150b8fb47b32554f629e92d358ce922
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-26 14:59:27 +0100

    Add test for modal close action

--- src/app/core/modal/modal.component.spec.ts
+++ src/app/core/modal/modal.component.spec.ts
@@ -1,5 +1,7 @@
 import { provideMockStore, MockStore } from "@ngrx/store/testing";
 import { render, screen } from "@testing-library/angular";
+import { createMock } from "@testing-library/angular/jest-utils";
+import userEvent from "@testing-library/user-event";
 import { ApplicationState } from "src/app/store/types";
 import { UiService } from "../../services/ui/ui.service";
 import { Modal, UiState } from "../../store/ui-feature/types";
@@ -7,24 +9,25 @@ import { ButtonComponent } from "../button/button.component";
 import { ModalComponent } from "./modal.component";
 
 describe("Given a modal component", () => {
+  const errorModal: Modal = {
+    isError: true,
+    modalText: "There was an error",
+  };
+
+  const mockUiState: UiState = {
+    isLoading: false,
+    showModal: true,
+    modalInformation: errorModal,
+  };
+
+  const initialState = {
+    ui: mockUiState,
+  };
+  const buttonText = /close/i;
+
   describe("When it is rendered and the store has modal information isError true and modalText 'There was an error'", () => {
     test("Then it should show 'Error', 'There was an error' and a button 'Close' on the screen", async () => {
-      const buttonText = /close/i;
       const modalTitle = /^error$/i;
-      const errorModal: Modal = {
-        isError: true,
-        modalText: "There was an error",
-      };
-
-      const mockUiState: UiState = {
-        isLoading: false,
-        showModal: true,
-        modalInformation: errorModal,
-      };
-
-      const initialState = {
-        ui: mockUiState,
-      };
 
       await render(ModalComponent, {
         providers: [UiService, provideMockStore({ initialState })],
@@ -69,6 +72,26 @@ describe("Given a modal component", () => {
 
       expect(renderedModalTitle).toBeInTheDocument();
       expect(renderedModalText).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and the user clicks close", () => {
+    test("Then uiService's hideModal method should be invoked", async () => {
+      const uiService = createMock(UiService);
+
+      await render(ModalComponent, {
+        providers: [provideMockStore({ initialState })],
+        declarations: [ButtonComponent],
+        componentProviders: [{ provide: UiService, useValue: uiService }],
+      });
+
+      const renderedButton = screen.queryByRole("button", {
+        name: buttonText,
+      });
+
+      await userEvent.click(renderedButton!);
+
+      expect(uiService.hideModal).toHaveBeenCalled();
     });
   });
 });
