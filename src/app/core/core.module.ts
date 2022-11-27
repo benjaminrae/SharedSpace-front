73af32e45e9ba25a8a3546bd3a935fe1e4af6767
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-27 16:39:36 +0100

    Add conditional rendering to header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from "@angular/common";
 import { LoadingComponent } from "./loading/loading.component";
 import { ButtonComponent } from "./button/button.component";
 import { ModalComponent } from "./modal/modal.component";
+import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from "../app-routing.module";
 
 @NgModule({
-  declarations: [LoadingComponent, ButtonComponent, ModalComponent],
-  imports: [CommonModule],
-  exports: [LoadingComponent, ButtonComponent, ModalComponent],
+  declarations: [
+    LoadingComponent,
+    ButtonComponent,
+    ModalComponent,
+    HeaderComponent,
+  ],
+  imports: [CommonModule, AppRoutingModule],
+  exports: [LoadingComponent, ButtonComponent, ModalComponent, HeaderComponent],
 })
 export class CoreModule {}
