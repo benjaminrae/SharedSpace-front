dde561d5813a21016caba3d63df7c08f600d513c
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-27 11:16:37 +0100

    Implement routing in credentials page test to test /login and /register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, type Routes } from "@angular/router";
 import { RegisterFormComponent } from "./components/register-form/register-form.component";
 import { CredentialsPageComponent } from "./pages/credentials-page/credentials-page.component";
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" },
   { path: "login", component: CredentialsPageComponent },
   { path: "register", component: CredentialsPageComponent },
--- src/app/pages/credentials-page/credentials-page.component.spec.ts
+++ src/app/pages/credentials-page/credentials-page.component.spec.ts
@@ -5,13 +5,20 @@ import { provideMockStore } from "@ngrx/store/testing";
 import { render, screen } from "@testing-library/angular";
 import { ButtonComponent } from "../../core/button/button.component";
 import { LoginFormComponent } from "../../components/login-form/login-form.component";
-
+import { RouterTestingModule } from "@angular/router/testing";
 import { CredentialsPageComponent } from "./credentials-page.component";
+import { routes } from "../../app-routing.module";
+import { Router } from "@angular/router";
+import { RegisterFormComponent } from "../../components/register-form/register-form.component";
 
 const providers = [FormBuilder, HttpClient, HttpHandler, provideMockStore({})];
-const declarations = [LoginFormComponent, ButtonComponent];
+const declarations = [
+  RegisterFormComponent,
+  LoginFormComponent,
+  ButtonComponent,
+];
 
-describe("Given the LoginPageComponent", () => {
+describe("Given the CredentialsPageComponent", () => {
   let component: CredentialsPageComponent;
   let fixture: ComponentFixture<CredentialsPageComponent>;
 
@@ -29,25 +36,111 @@ describe("Given the LoginPageComponent", () => {
     test("Then the component should be created", () => {
       expect(component).toBeTruthy();
     });
-
-    test("and then it should match the snapshot", () => {
-      expect(component).toMatchSnapshot();
-    });
   });
 });
 
-describe("Given the LoginPage component", () => {
-  describe("When it is rendered", () => {
-    test("Then it should show an image on the screen with name 'A man working at a desk' and a heading level 2 with 'Log in to SharedSpace'", async () => {
+describe("Given the CredentialsPage component", () => {
+  describe("When it is rendered and the path is /login", () => {
+    test("Then it should match the snapshot", async () => {
+      const { fixture, detectChanges } = await render(
+        CredentialsPageComponent,
+        {
+          declarations,
+          providers,
+          imports: [RouterTestingModule.withRoutes(routes)],
+          detectChanges: false,
+        }
+      );
+
+      const router = TestBed.inject(Router);
+
+      await fixture.ngZone!.run(async () => router.navigate(["/login"]));
+
+      detectChanges();
+
+      expect(fixture).toMatchSnapshot();
+    });
+
+    test("And then it should show an image on the screen with name 'A man working at a desk' and a heading level 2 with 'Log in to SharedSpace'", async () => {
       const imageAltText = "A man working at a desk";
       const loginHeading = {
         level: 2,
         name: "Log in to SharedSpace",
       };
 
-      await render(CredentialsPageComponent, { declarations, providers });
+      const { fixture, detectChanges } = await render(
+        CredentialsPageComponent,
+        {
+          declarations,
+          providers,
+          imports: [RouterTestingModule.withRoutes(routes)],
+          detectChanges: false,
+        }
+      );
+
+      const router = TestBed.inject(Router);
+
+      await fixture.ngZone!.run(async () => router.navigate(["/login"]));
+
+      detectChanges();
+      screen.debug();
 
       const renderedHeading = screen.queryByRole("heading", loginHeading);
+      const renderedImage = screen.queryByRole("img", {
+        name: imageAltText,
+      });
+
+      expect(renderedHeading).toBeInTheDocument();
+      expect(renderedImage).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and the path is /register", () => {
+    test("Then it should match the snapshot", async () => {
+      const { fixture, detectChanges } = await render(
+        CredentialsPageComponent,
+        {
+          declarations,
+          providers,
+          imports: [RouterTestingModule.withRoutes(routes)],
+          detectChanges: false,
+        }
+      );
+
+      const router = TestBed.inject(Router);
+
+      await fixture.ngZone!.run(async () => router.navigate(["/register"]));
+
+      detectChanges();
+
+      expect(fixture).toMatchSnapshot();
+    });
+
+    test("And then it should show an image on the screen with name 'A man working at a desk' and a heading level 2 with 'Sign up for SharedSpace'", async () => {
+      const imageAltText = "A man working at a desk";
+      const signupHeading = {
+        level: 2,
+        name: "Sign up for SharedSpace",
+      };
+
+      const { fixture, detectChanges } = await render(
+        CredentialsPageComponent,
+        {
+          declarations,
+          providers,
+          imports: [RouterTestingModule.withRoutes(routes)],
+          detectChanges: false,
+        }
+      );
+
+      const router = TestBed.inject(Router);
+
+      await fixture.ngZone!.run(async () => router.navigate(["/register"]));
+
+      detectChanges();
+      screen.debug();
+
+      const renderedHeading = screen.queryByRole("heading", signupHeading);
       const renderedImage = screen.queryByRole("img", {
         name: imageAltText,
       });
