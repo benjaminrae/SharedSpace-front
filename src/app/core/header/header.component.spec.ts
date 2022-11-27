fb9be48438a621945cc240724f7123391e20023e
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-27 17:51:27 +0100

    Change logout to button and test onClick

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,11 +1,20 @@
 import { HttpClient, HttpHandler } from "@angular/common/http";
+import { TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
 import { provideMockStore } from "@ngrx/store/testing";
-import { render, screen } from "@testing-library/angular";
+import { fireEvent, render, screen } from "@testing-library/angular";
+import { createMock } from "@testing-library/angular/jest-utils";
+import userEvent from "@testing-library/user-event";
+import { routes } from "../../app-routing.module";
+import { TokenService } from "../../services/token/token.service";
+import { UserService } from "../../services/user/user.service";
 import mockInitialUiState from "../../mocks/states/mockInitialUiState";
 import mockInitialUserState from "../../mocks/states/mockInitialUserState";
 import { ApplicationState } from "../../store/types";
 import { UserState } from "../../store/user-feature/types";
 import { HeaderComponent } from "./header.component";
+import { Observable, of, Subject } from "rxjs";
 
 describe("Given a Header component", () => {
   const homeLabel = /home/i;
@@ -42,14 +51,13 @@ describe("Given a Header component", () => {
   });
 
   describe("When it is rendered and a user is logged in", () => {
+    const logoutLabel = /logout/i;
+    const loggedUserState: UserState = {
+      ...mockInitialUserState,
+      isLogged: true,
+    };
+
     test("Then it should show a heading level 1 'SharedSpace', and links 'Home' and 'Logout'", async () => {
-      const logoutLabel = /logout/i;
-
-      const loggedUserState: UserState = {
-        ...mockInitialUserState,
-        isLogged: true,
-      };
-
       await render(HeaderComponent, {
         providers: [
           HttpClient,
@@ -68,11 +76,52 @@ describe("Given a Header component", () => {
         level: 1,
         name: expectedHeading,
       });
-      const logoutLink = screen.queryByRole("link", { name: logoutLabel });
+      const logoutButton = screen.queryByRole("button", { name: logoutLabel });
 
       expect(homeLink).toBeInTheDocument();
       expect(renderedHeading).toBeInTheDocument();
-      expect(logoutLink).toBeInTheDocument();
+      expect(logoutButton).toBeInTheDocument();
+    });
+
+    test("And then userService's logoutUser and tokenService's removeToken should be called when Logout is clicked and the user should navigate", async () => {
+      const userService = createMock(UserService);
+      const tokenService = createMock(TokenService);
+
+      userService.getIsLogged.mockReturnValue(of({ isLogged: true }));
+
+      await render(HeaderComponent, {
+        providers: [
+          HttpClient,
+          HttpHandler,
+          provideMockStore<ApplicationState>({
+            initialState: {
+              ui: mockInitialUiState,
+              user: loggedUserState,
+            },
+          }),
+        ],
+        componentProviders: [
+          {
+            provide: UserService,
+            useValue: userService,
+          },
+          { provide: TokenService, useValue: tokenService },
+        ],
+        imports: [RouterTestingModule.withRoutes(routes)],
+      });
+
+      const logoutButton = screen.queryByRole("button", {
+        name: logoutLabel,
+      });
+
+      const router = TestBed.inject(Router);
+
+      await userEvent.click(logoutButton!);
+
+      expect(logoutButton).toBeInTheDocument();
+      expect(router.navigated).toBe(true);
+      expect(userService.logoutUser).toHaveBeenCalled();
+      expect(tokenService.removeToken).toHaveBeenCalled();
     });
   });
 });
