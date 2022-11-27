dc7dfc4606456924bb8cdf79ee1d5eea46e4c53f
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-27 12:52:06 +0100

    Create not-found-page html and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, type Routes } from "@angular/router";
 import { CredentialsPageComponent } from "./pages/credentials-page/credentials-page.component";
+import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
 
 export const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" },
   { path: "login", component: CredentialsPageComponent },
   { path: "register", component: CredentialsPageComponent },
+  { path: "**", component: NotFoundPageComponent },
 ];
 
 @NgModule({
