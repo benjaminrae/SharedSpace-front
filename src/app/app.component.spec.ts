9006c993ccdf67f01b3cf0edcce4a1f1a427790c
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-28 09:39:08 +0100

    Add test for ngOnInit in app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,15 +6,31 @@ import { TokenService } from "./services/token/token.service";
 import { UserService } from "./services/user/user.service";
 import { HttpClient, HttpHandler } from "@angular/common/http";
 import { HeaderComponent } from "./core/header/header.component";
+import { routes } from "./app-routing.module";
+import mockToken from "./mocks/states/mockToken";
+import { ApplicationState } from "./store/types";
+import mockInitialUiState from "./mocks/states/mockInitialUiState";
+import mockInitialUserState from "./mocks/states/mockInitialUserState";
+import { CredentialsPageComponent } from "./pages/credentials-page/credentials-page.component";
+import { ButtonComponent } from "./core/button/button.component";
+import mockLocalStorage from "./mocks/states/localStorage/mockLocalStorage";
+import { createMock } from "@testing-library/angular/jest-utils";
+
+beforeAll(() => {
+  Object.defineProperty(window, "localStorage", {
+    value: mockLocalStorage,
+  });
+});
 
 const providers = [
   UiService,
   TokenService,
-  UiService,
   UserService,
   HttpClient,
   HttpHandler,
-  provideMockStore({}),
+  provideMockStore<ApplicationState>({
+    initialState: { ui: mockInitialUiState, user: mockInitialUserState },
+  }),
 ];
 
 describe("Given an AppComponent", () => {
@@ -33,6 +49,49 @@ describe("Given an AppComponent", () => {
       const renderedHeading = screen.getByRole("heading", expectedHeading);
 
       expect(renderedHeading).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and the user navigates to '/register'", () => {
+    test("Then the user should see the register form on the screen", async () => {
+      await render(AppComponent, {
+        providers,
+        declarations: [HeaderComponent],
+        routes,
+      });
+
+      screen.debug();
+    });
+  });
+
+  describe("When it is rendered and there is a token in local storage", () => {
+    mockLocalStorage.setItem("token", mockToken);
+    const userService = createMock(UserService);
+    const tokenService = new TokenService();
+
+    const user = tokenService.decodeToken(mockToken);
+
+    test("Then the user should be logged in", async () => {
+      const { fixture, detectChanges } = await render(AppComponent, {
+        providers,
+        declarations: [
+          HeaderComponent,
+          CredentialsPageComponent,
+          ButtonComponent,
+        ],
+        componentProviders: [
+          {
+            provide: UserService,
+            useValue: userService,
+          },
+        ],
+        routes,
+      });
+
+      expect(userService.loginUser).toHaveBeenCalledWith({
+        ...user,
+        token: mockToken,
+      });
     });
   });
 });
