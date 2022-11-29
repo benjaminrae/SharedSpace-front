49732856cd48c11e323eda54f882467db987f948
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-29 21:12:40 +0100

    Update reducers in store to include locations

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,5 +1,6 @@
 import { type ActionReducerMap, type MetaReducer } from "@ngrx/store";
 import { environment } from "../../environments/environment";
+import { locationsFeature } from "./locations-feature/locations-feature.reducer";
 import { type ApplicationState } from "./types";
 import { uiFeature } from "./ui-feature/ui-feature.reducer";
 import { userFeature } from "./user-feature/user-feature.reducer";
@@ -7,6 +8,7 @@ import { userFeature } from "./user-feature/user-feature.reducer";
 export const reducers: ActionReducerMap<ApplicationState> = {
   ui: uiFeature.reducer,
   user: userFeature.reducer,
+  locations: locationsFeature.reducer,
 };
 
 export const metaReducers: Array<MetaReducer<ApplicationState>> =
