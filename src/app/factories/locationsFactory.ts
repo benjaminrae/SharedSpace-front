2a4e2239c8edca1af8fd613d529765ef32e3f2a6
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-29 17:36:57 +0100

    Add location factory

---
+++ src/app/factories/locationsFactory.ts
@@ -0,0 +1,36 @@
+import { Factory } from "fishery";
+import { faker } from "@faker-js/faker";
+import { LocationStructure } from "../store/locations-feature/types";
+
+const locationFactory = Factory.define<LocationStructure>(() => ({
+  name: faker.company.name(),
+  location: faker.address.nearbyGPSCoordinate().toString(),
+  owner: faker.database.mongodbObjectId(),
+  images: {
+    image: faker.image.business(),
+    small: faker.image.business(),
+    backup: faker.image.business(),
+    backupSmall: faker.image.business(),
+  },
+  services: {
+    airConditioning: faker.datatype.boolean(),
+    allDayAccess: faker.datatype.boolean(),
+    eventManagement: faker.datatype.boolean(),
+    freeTeaCoffee: faker.datatype.boolean(),
+    freeTrial: faker.datatype.boolean(),
+    kitchen: faker.datatype.boolean(),
+    meetingRoom: faker.datatype.boolean(),
+    parking: faker.datatype.boolean(),
+    photocopier: faker.datatype.boolean(),
+    printer: faker.datatype.boolean(),
+    projector: faker.datatype.boolean(),
+    reception: faker.datatype.boolean(),
+    scanner: faker.datatype.boolean(),
+    tv: faker.datatype.boolean(),
+    whiteboard: faker.datatype.boolean(),
+    wifi: faker.datatype.boolean(),
+  },
+}));
+
+export const getRandomLocations = (number: number) =>
+  locationFactory.buildList(number);
