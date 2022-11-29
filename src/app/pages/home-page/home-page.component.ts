c60d36f21837ccd4293d268847787320850bf684
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-29 19:59:56 +0100

    Prepare home page to show cards

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { getRandomLocations } from "src/app/factories/locationsFactory";
+import { Locations } from "src/app/store/locations-feature/types";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.scss"],
 })
 export class HomePageComponent {
+  locations!: Locations;
 
+  constructor() {
+    this.locations = getRandomLocations(20);
+  }
 }
