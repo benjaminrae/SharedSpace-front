f600279bdd61fff1d66294f81738daf1616f55a1
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-29 18:33:39 +0100

    Test handle error method in location services

--- src/app/services/locations/locations.service.spec.ts
+++ src/app/services/locations/locations.service.spec.ts
@@ -8,13 +8,16 @@ import { provideMockStore } from "@ngrx/store/testing";
 import { getRandomLocations } from "../../factories/locationsFactory";
 import { environment } from "../../../environments/environment.prod";
 import { LocationsService } from "./locations.service";
+import { createMock } from "@testing-library/angular/jest-utils";
+import { UiService } from "../ui/ui.service";
+import { throwError } from "rxjs";
 
 const { apiUrl } = environment;
 
 const providers = [provideMockStore({})];
 
 const serverErrorMessage = "There was an error on the server";
-const clientErrorMessage = "Incorrect username or password";
+const clientErrorMessage = "Bad request";
 const serverError: Partial<HttpErrorResponse> = {
   message: serverErrorMessage,
   error: {
@@ -92,6 +95,30 @@ describe("Given the service Locations Service", () => {
       httpMock.verify();
 
       expect(handleError).toHaveBeenCalled();
+    });
+  });
+
+  describe("When the method handleError is invoked with a HttpErrorResponse with 'Bad request' and the UiService", () => {
+    test("Then UiService's showLoading and showErrorModal with the received error should be invoked and it should return a thrown error", () => {
+      const uiService = createMock(UiService);
+
+      service.handleError(clientError as HttpErrorResponse, uiService);
+
+      expect(uiService.hideLoading).toHaveBeenCalled();
+      expect(uiService.showErrorModal).toHaveBeenCalledWith(clientErrorMessage);
+      expect(throwError).toHaveBeenCalled();
+    });
+  });
+
+  describe("When the method handleError is invoked with a HttpErrorResponse with 'There was an error on the server' and the UiService", () => {
+    test("Then UiService's showLoading and showErrorModal with the received error should be invoked and it should return a thrown error", () => {
+      const uiService = createMock(UiService);
+
+      service.handleError(serverError as HttpErrorResponse, uiService);
+
+      expect(uiService.hideLoading).toHaveBeenCalled();
+      expect(uiService.showErrorModal).toHaveBeenCalledWith(serverErrorMessage);
+      expect(throwError).toHaveBeenCalled();
     });
   });
 });
