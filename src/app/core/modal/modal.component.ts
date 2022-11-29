18ff37d40c7fa5aa3f9372e0bfd98479d4e4bbd3
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-29 15:45:34 +0100

    Add focus to modal on view init

--- src/app/core/modal/modal.component.ts
+++ src/app/core/modal/modal.component.ts
@@ -1,18 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild, AfterViewInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { UiService } from "../../services/ui/ui.service";
 import { Modal } from "../../store/ui-feature/types";
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: "app-modal",
   templateUrl: "./modal.component.html",
   styleUrls: ["./modal.component.scss"],
 })
-export class ModalComponent {
+export class ModalComponent implements AfterViewInit {
+  @ViewChild(ButtonComponent) component!: ButtonComponent;
+  buttonRef!: ElementRef<HTMLButtonElement>;
+
   modalInformation$!: Observable<Modal>;
 
   constructor(private readonly uiService: UiService) {
     this.modalInformation$ = uiService.getModalInformation();
+  }
+
+  ngAfterViewInit(): void {
+    this.component.button.nativeElement.focus();
   }
 
   onClick() {
