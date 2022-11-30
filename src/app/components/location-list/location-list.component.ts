d9c35cccee5b6661e076a28f31ae1d75845f2433
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-30 13:12:40 +0100

    Move list in home page to location list component

---
+++ src/app/components/location-list/location-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+import { Observable } from "rxjs";
+import { Locations } from "src/app/store/locations-feature/types";
+
+@Component({
+  selector: "app-location-list",
+  templateUrl: "./location-list.component.html",
+  styleUrls: ["./location-list.component.scss"],
+})
+export class LocationListComponent {
+  @Input() locations$!: Observable<Locations>;
+}
