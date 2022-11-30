119214ff326eb18eec712884c78c16ea8c6d36bc
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-30 20:07:26 +0100

    Add test for form controls

--- src/app/components/location-form/location-form.component.spec.ts
+++ src/app/components/location-form/location-form.component.spec.ts
@@ -1,23 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from "@angular/forms";
+import { render, screen } from "@testing-library/angular";
 
-import { LocationFormComponent } from './location-form.component';
+import { LocationFormComponent } from "./location-form.component";
 
-describe('LocationFormComponent', () => {
-  let component: LocationFormComponent;
-  let fixture: ComponentFixture<LocationFormComponent>;
+describe("Given a LocationFormComponent", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a name, location description and image input", async () => {
+      const nameLabel = /name/i;
+      const locationLabel = /location/i;
+      const descriptionLabel = /description/i;
+      const imageLabel = /image/i;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LocationFormComponent ]
-    })
-    .compileComponents();
+      await render(LocationFormComponent, {
+        providers: [FormBuilder],
+      });
 
-    fixture = TestBed.createComponent(LocationFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      const nameInput = screen.queryByRole("textbox", {
+        name: nameLabel,
+      });
+      const locationInput = screen.queryByRole("textbox", {
+        name: locationLabel,
+      });
+      const descriptionInput = screen.queryByRole("textbox", {
+        name: descriptionLabel,
+      });
+      const imageInput = screen.queryByLabelText(imageLabel);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+      expect(nameInput).toBeInTheDocument();
+      expect(locationInput).toBeInTheDocument();
+      expect(descriptionInput).toBeInTheDocument();
+      expect(imageInput).toBeInTheDocument();
+    });
+
+    test("And it should show 15 checkboxes", async () => {
+      const totalCheckboxes = 15;
+
+      await render(LocationFormComponent, {
+        providers: [FormBuilder],
+      });
+
+      const checkboxes = screen.queryAllByRole("checkbox");
+
+      expect(checkboxes).toHaveLength(totalCheckboxes);
+    });
   });
 });
