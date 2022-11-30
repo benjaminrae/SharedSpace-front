e84e2879a65d898d9230803f15c8ead5a7283df1
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-30 20:45:13 +0100

    Add test for image upload and preview

--- src/app/components/location-form/location-form.component.spec.ts
+++ src/app/components/location-form/location-form.component.spec.ts
@@ -1,16 +1,19 @@
 import { FormBuilder } from "@angular/forms";
 import { render, screen } from "@testing-library/angular";
+import userEvent from "@testing-library/user-event/";
 import { ButtonComponent } from "../../core/button/button.component";
 
 import { LocationFormComponent } from "./location-form.component";
 
 describe("Given a LocationFormComponent", () => {
+  const imageAlt = /your image/i;
+  const imageLabel = /image/i;
+
   describe("When it is rendered", () => {
     test("Then it should show a name, location description and image input", async () => {
       const nameLabel = /name/i;
       const locationLabel = /location/i;
       const descriptionLabel = /description/i;
-      const imageLabel = /image/i;
 
       await render(LocationFormComponent, {
         providers: [FormBuilder],
@@ -62,17 +65,32 @@ describe("Given a LocationFormComponent", () => {
     });
 
     test("And it should show an image with alt 'Your image'", async () => {
-      const imageAlt = /your image/i;
+      await render(LocationFormComponent, {
+        providers: [FormBuilder],
+        declarations: [ButtonComponent],
+      });
+
+      const renderedImage = screen.queryByAltText(imageAlt);
+
+      expect(renderedImage).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and the user uploads an image", () => {
+    test("Then it should show a preview of the image", async () => {
+      const image = new File(["image"], "image.jpg", { type: "image/jpg" });
 
       await render(LocationFormComponent, {
         providers: [FormBuilder],
         declarations: [ButtonComponent],
       });
 
-      screen.debug();
       const renderedImage = screen.queryByAltText(imageAlt);
+      const imageInput = screen.queryByLabelText(imageLabel);
 
-      expect(renderedImage).toBeInTheDocument();
+      await userEvent.upload(imageInput!, image);
+
+      expect((renderedImage as HTMLImageElement).src).toBe("http://localhost/");
     });
   });
 });
