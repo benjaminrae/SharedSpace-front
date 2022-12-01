f60ebdce299f84d32b8eb10a13435e7b42dfb07c
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-12-01 18:09:47 +0100

    Create basic my locations page

--- src/app/pages/my-locations-page/my-locations-page.component.ts
+++ src/app/pages/my-locations-page/my-locations-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { LocationsService } from "src/app/services/locations/locations.service";
+import { Locations } from "src/app/store/locations-feature/types";
 
 @Component({
-  selector: 'app-my-locations-page',
-  templateUrl: './my-locations-page.component.html',
-  styleUrls: ['./my-locations-page.component.scss']
+  selector: "app-my-locations-page",
+  templateUrl: "./my-locations-page.component.html",
+  styleUrls: ["./my-locations-page.component.scss"],
 })
 export class MyLocationsPageComponent {
+  locations$!: Observable<Locations>;
+  count$!: Observable<number>;
 
+  constructor(private readonly locationsService: LocationsService) {
+    locationsService.getMyLocations();
+    this.locations$ = locationsService.selectLocations();
+    this.count$ = locationsService.selectCount();
+  }
 }
