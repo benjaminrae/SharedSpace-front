7fe7cd9b8d44121d661fc05c51ccb724dca875a0
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-12-04 22:20:21 +0100

    Create filter component

---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,70 @@
+import { Component } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { LocationsService } from "src/app/services/locations/locations.service";
+
+@Component({
+  selector: "app-filter",
+  templateUrl: "./filter.component.html",
+  styleUrls: ["./filter.component.scss"],
+})
+export class FilterComponent {
+  isOpen = false;
+  services = this.fb.group({
+    airConditioning: false,
+    allDayAccess: false,
+    eventManagement: false,
+    freeTeaCoffee: false,
+    freeTrial: false,
+    kitchen: false,
+    meetingRoom: false,
+    parking: false,
+    photocopier: false,
+    printer: false,
+    projector: false,
+    reception: false,
+    scanner: false,
+    tv: false,
+    whiteboard: false,
+    wifi: false,
+  });
+
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly locationsService: LocationsService
+  ) {}
+
+  toggleFilter() {
+    this.isOpen = !this.isOpen;
+  }
+
+  resetFilters() {
+    this.services.setValue({
+      airConditioning: false,
+      allDayAccess: false,
+      eventManagement: false,
+      freeTeaCoffee: false,
+      freeTrial: false,
+      kitchen: false,
+      meetingRoom: false,
+      parking: false,
+      photocopier: false,
+      printer: false,
+      projector: false,
+      reception: false,
+      scanner: false,
+      tv: false,
+      whiteboard: false,
+      wifi: false,
+    });
+
+    this.filterChange();
+  }
+
+  filterChange() {
+    const keys = Object.entries(this.services.value)
+      .filter(([, value]) => value === true)
+      .map(([key]) => key);
+
+    this.locationsService.getFilteredLocations(`?services=${keys.join(",")}`);
+  }
+}
