577517bf492cc1161cb9d787a7076b245c67f831
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-12-05 00:58:50 +0100

    Add delete button click listener and test

--- src/app/pages/location-page/location-page.component.spec.ts
+++ src/app/pages/location-page/location-page.component.spec.ts
@@ -4,22 +4,25 @@ import { provideMockStore } from "@ngrx/store/testing";
 import { render, screen } from "@testing-library/angular";
 import { createMock } from "@testing-library/angular/jest-utils";
 import { of } from "rxjs";
+import userEvent from "@testing-library/user-event";
 import { getRandomLocation } from "../../factories/locationsFactory";
 import { LocationsService } from "../../services/locations/locations.service";
 import { UiService } from "../../services/ui/ui.service";
 import { UserService } from "../../services/user/user.service";
 import { LocationPageComponent } from "./location-page.component";
+import { ButtonComponent } from "../../core/button/button.component";
 
 describe("Given a location page component", () => {
+  const locationService = createMock(LocationsService);
+  const userService = createMock(UserService);
+  const uiService = createMock(UiService);
+  const location = getRandomLocation();
+  const fakeActivatedRoute: Partial<ActivatedRoute> = {
+    params: of({ locationId: location.id }),
+  };
+
   describe("When it is rendered with a location", () => {
     test("Then getLocationById, showLoading and getUserId should be called and the location's name in a heading level 2", async () => {
-      const locationService = createMock(LocationsService);
-      const userService = createMock(UserService);
-      const uiService = createMock(UiService);
-      const location = getRandomLocation();
-      const fakeActivatedRoute: Partial<ActivatedRoute> = {
-        params: of({ locationId: location.id }),
-      };
       const expectedHeading = { level: 2, name: location.name };
 
       locationService.getLocationById = jest
@@ -42,6 +45,37 @@ describe("Given a location page component", () => {
       expect(userService.getUserId).toHaveBeenCalled();
       expect(uiService.showLoading).toHaveBeenCalled();
       expect(renderedHeading).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered with a location and the delete button is clicked", () => {
+    test("Then deleteLocation should be called with the location's id and navigate with '/'", async () => {
+      const deleteLabel = /delete/i;
+      locationService.getLocationById = jest
+        .fn()
+        .mockReturnValue(of({ location }));
+
+      userService.getUserId = jest.fn().mockReturnValue(of(location.owner));
+
+      await render(LocationPageComponent, {
+        providers: [HttpClient, HttpHandler, provideMockStore({})],
+        componentProviders: [
+          { provide: LocationsService, useValue: locationService },
+          { provide: UserService, useValue: userService },
+          { provide: UiService, useValue: uiService },
+          { provide: ActivatedRoute, useValue: fakeActivatedRoute },
+        ],
+        declarations: [ButtonComponent],
+      });
+
+      const deleteButton = screen.queryByRole("button", {
+        name: deleteLabel,
+      });
+
+      await userEvent.click(deleteButton!);
+
+      expect(locationService.deleteLocation).toHaveBeenCalledWith(location.id);
+      expect(uiService.navigate).toHaveBeenCalledWith("/");
     });
   });
 });
